c88c7c382924b7f26e203d0c1421da8d0fd0cdd2
Gastonm1 <gaston.marques@gmail.com>
2019-03-12 21:20:15 -0500

    first method

--- db/config/orm.js
+++ db/config/orm.js
@@ -3,3 +3,15 @@ var connection = require ("../config/connection");
 //-------------------------------------------------------------------
 
 // Create the methods that will execute the MySQL commands needed to retrieve and store data into database.
+
+var orm = {
+    selectAll : function() {
+        return connection.query("SELECT * FROM burgers", function (err,res){
+            if (err) throw err;
+            console.log("This is All of the burgers")
+            console.log(res);
+        })
+    }
+}
+
+module.exports = orm;
