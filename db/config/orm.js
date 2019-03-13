f5b66bc374dcd7dfaf00af5230f1248b90e8d7e0
Gastonm1 <gaston.marques@gmail.com>
2019-03-12 22:24:23 -0500

    methods

--- db/config/orm.js
+++ db/config/orm.js
@@ -11,7 +11,30 @@ var orm = {
             console.log("This is All of the burgers")
             console.log(res);
         })
-    }
+    },
+
+
+//---------------------------------------------------------------------------------------------------------
+// ASK TA how to properly test.... I feel like these are very close but will receive error codes... 
+//---------------------------------------------------------------------------------------------------------
+    // insertOne : function (burger_name, devoured){
+    //     var queryString = "INSERT INTO burgers";
+    //     console.log("This is adding new burgers to DB")
+    //     return connection.query(querystring, [burger_name,devoured], function(err,res) {
+    //         if (err) throw err;
+    //         // insertOne(res);
+    //         console.log(insertOne(res))
+    //     }
+    // )},
+
+    // updateOne : function() {
+    //     var queryString = "UPDATE burgers SET ?? WHERE ?? = ?";
+    //     console.log("This is updating a existing burger from DB");
+    //     return connection.query(queryString, [burger_name, burgers], function (err, res){
+    //         if (err) throw err;
+    //         updateOne (res);
+    //     })
+    // }
 }
 
 module.exports = orm;
