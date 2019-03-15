fa4f28341622112efe133aaecc152df62e4190e9
Gastonm1 <gaston.marques@gmail.com>
2019-03-14 20:46:50 -0500

    burgers route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,4 +5,18 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions
 var burgers = require ("../models/burger");
 
-router.get("/", 
+router.get("/", function(req, res) {
+    burgers.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burgers.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(res){
+        res.json({id: res.insertID});
+    });
+});
